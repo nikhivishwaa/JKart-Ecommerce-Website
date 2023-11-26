8864751fbc7d21af652cc12aef30b2ec2bb29d1f
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-11-26 15:33:39 +0530

    Carousal added

--- jkart/shop/models.py
+++ jkart/shop/models.py
@@ -5,4 +5,12 @@ class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
     product_description = models.CharField(max_length=300)
+    category = models.CharField(max_length=30,default="")  # default argument fill the null fields of prev table
+    sub_category = models.CharField(max_length=50,default="")
     publish_date = models.DateField()
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='shop/images',default="")
+
+    def __str__(self):
+        return self.product_name
+    
--- jkart/shop/views.py
+++ jkart/shop/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 def index(request):
-    return render(request, 'shop/index.html')
+    # if request.method == 'POST':
+    #     print(request.POST.get('search'))
+
+    products = Product.objects.all()
+    n = len(products)
+    no_of_slides = n//4 + ceil(n/4 - n//4)
+    params = {"no_of_slides" : no_of_slides, "range": range(1, no_of_slides), "products": products}
+    return render(request, 'shop/index.html', params)
 
 def offers(request):
-    return HttpResponse('shop offers page')
+    return render(request, 'shop/offers.html')
 
 def prodView(request):
     return HttpResponse('shop prodView page')
