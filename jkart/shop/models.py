e4c7609871d1d391c7bae30013a9224130c080e9
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-11 19:32:59 +0530

    Contact model added successfully

--- jkart/shop/models.py
+++ jkart/shop/models.py
@@ -14,3 +14,15 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
     
+class Contact(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, default="")
+    desc = models.CharField(max_length=500, default="")
+    phone = models.PositiveBigIntegerField(default=0)
+    email = models.EmailField(default="", max_length=50)
+    read_status = models.CharField(max_length=6,default="unread")
+    msg_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " | Recieved on: " +str(self.msg_date)
+    
