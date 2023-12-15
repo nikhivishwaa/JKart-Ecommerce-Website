c224c4aff6212b5c189157d8a83b43a94449d5f9
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-15 10:21:43 +0530

    quantity adjust button added on Jkart with fully functionality

--- jkart/shop/static/shop/JS/cart.js
+++ jkart/shop/static/shop/JS/cart.js
@@ -31,58 +31,61 @@ function prevSlide() {
 
 if (localStorage.getItem('cart') == null) {
     var cart = {};
-    console.log("cart");
 }
 else {
     cart = JSON.parse(localStorage.getItem('cart'));
-    console.log(cart);
 }
 
 // JQuery
-$('.cart').click(function () {
-    console.log("clicked");
+$('.divpr').on('click', 'button.cart', function () {
     let additem = this.id.toString();
-    console.log(additem);
 
     if (cart[additem] != undefined) {
         cart[additem] += 1;
-        qtyButton(additem);
+        qtyButton();
     }
     else {
         cart[additem] = 1;
-        qtyButton(additem);
+        qtyButton();
     }
-    console.log(cart);
     localStorage.setItem("cart", JSON.stringify(cart));
 });
 const qtyButton = () => {
     for (const key in cart) {
-        let a = key.slice(2,);
-        var quantity_btn = `<button class='btn minus' id='minus${a}'>-</button><span id='qty${a}'>${cart['pr' + a]}</span><button class='btn plus' id='plus${a}'>+</button>`;
-        document.getElementById(`atc${a}`).innerHTML = quantity_btn;
+        var quantity_btn = `<button class='btn minus' id='minus${key}'>-</button><span id='qty${key}'>${cart[key]}</span><button class='btn plus' id='plus${key}'>+</button>`;
+        document.getElementById(`div${key}`).innerHTML = quantity_btn;
     }
 }
 
-$('.minus').click(function () {
-    let id = this.id.slice(6,);
-    console.log(id, 'minus clicked');
-    cart['pr' + id] = Math.max(0, (cart['pr' + id] - 1));
-    let qty = cart['pr' + id];
+// if minus button is clicked
+$('.divpr').on('click', 'button.minus', function () {
+    let id = this.id.slice(5,);
+    cart[id] = Math.max(0, (cart[id] - 1));
+    if (!cart[id]) {
+        document.getElementById('div' + id).innerHTML = `<button class="btn cart" id="${id}">Add to Cart</button>`;
+        delete cart[id];
+    }
+    else document.getElementById(`qty${id}`).innerHTML = cart[id];
     localStorage.setItem('cart', JSON.stringify(cart));
-    document.getElementById(`qty${a}`).innerHTML = qty;
+});
+
+// if plus button is clicked
+$('.divpr').on('click', 'button.plus', function () {
+    let id = this.id.slice(4,);
+    cart[id] += 1;
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.getElementById(`qty${id}`).innerHTML = cart[id];
 });
 
 // Update total items count present in cart
 const updateCart = () => {
     let count = document.getElementsByClassName('count');
-    console.log(count);
     let _cart = Array.from(count);
     temp = JSON.parse(localStorage.getItem('cart'));
     let item = 0;
     for (const key in temp) {
         item += temp[key];
     }
-    console.log(item);
     for (const i of _cart) {
         i.innerHTML = item.toString();
     }
