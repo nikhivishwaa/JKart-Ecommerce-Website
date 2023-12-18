c020c170fd4aacaa2a0267b9d482185da15e47f1
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-18 21:30:07 +0530

    ckeckout page on the way pop over created and new ui for jkart

--- jkart/shop/static/shop/JS/cart.js
+++ jkart/shop/static/shop/JS/cart.js
@@ -13,11 +13,12 @@ $('.divpr').on('click', 'button.cart', function () {
     let additem = this.id.toString();
 
     if (cart[additem] != undefined) {
-        cart[additem] += 1;
+        cart[additem][0] += 1;
         adjustButton(additem);
     }
     else {
-        cart[additem] = 1;
+        let name = document.getElementById("title" + additem).innerHTML;
+        cart[additem] = [1, name];
         adjustButton(additem);
     }
     localStorage.setItem("cart", JSON.stringify(cart));
@@ -25,7 +26,7 @@ $('.divpr').on('click', 'button.cart', function () {
 
 const adjustButton = (key) => {
     if (cart[key] != undefined) {
-        var quantity_btn = `<button class='btn minus' id='minus${key}'>-</button><span id='qty${key}' class='value'>${cart[key]}</span><button class='btn plus' id='plus${key}'>+</button>`;
+        var quantity_btn = `<button class='btn minus' id='minus${key}'>-</button><span id='qty${key}' class='value'>${cart[key][0]}</span><button class='btn plus' id='plus${key}'>+</button>`;
         document.getElementById(`div${key}`).innerHTML = quantity_btn;
         updateCart();
     }
@@ -39,12 +40,12 @@ const qtyButton = () => {
 // if minus button is clicked
 $('.divpr').on('click', 'button.minus', function () {
     let id = this.id.slice(5,);
-    cart[id] = Math.max(0, (cart[id] - 1));
-    if (!cart[id]) {
+    cart[id][0] = Math.max(0, (cart[id][0] - 1));
+    if (!cart[id][0]) {
         document.getElementById('div' + id).innerHTML = `<button class="btn cart" id="${id}">Add to Cart</button>`;
         delete cart[id];
     }
-    else document.getElementById(`qty${id}`).innerHTML = cart[id];
+    else document.getElementById(`qty${id}`).innerHTML = cart[id][0];
     localStorage.setItem('cart', JSON.stringify(cart));
     updateCart();
 });
@@ -52,9 +53,9 @@ $('.divpr').on('click', 'button.minus', function () {
 // if plus button is clicked
 $('.divpr').on('click', 'button.plus', function () {
     let id = this.id.slice(4,);
-    cart[id] += 1;
+    cart[id][0] += 1;
     localStorage.setItem('cart', JSON.stringify(cart));
-    document.getElementById(`qty${id}`).innerHTML = cart[id];
+    document.getElementById(`qty${id}`).innerHTML = cart[id][0];
     updateCart();
 });
 
@@ -64,12 +65,34 @@ const updateCart = () => {
     let _cart = Array.from(count);
     let item = 0;
     for (const key in cart) {
-        item += cart[key];
+        item += cart[key][0];
     }
     for (const i of _cart) {
         i.innerHTML = item.toString();
     }
+    updatePopover();
 }
 
+const updatePopover = () => {
+    let x = document.getElementById('popover');
+    x.innerHTML = "";
+    let elem = 0;
+    for (const key in cart) {
+        let element = `<div class="prod"><div>${cart[key][1].slice(0, 15)}</div><div>|</div><div>Qty. ${cart[key][0]}</div></div>`;
+        x.innerHTML += element;
+        elem++;
+    }
+    if (elem) {
+        let btns = `<div class="popover-button"><button class="btn" onclick="foo()">Clear Cart</button><a href="/shop/checkout"><button class="btn">Checkout</button></a><div>`
+        x.innerHTML += btns;
+    }
+    else x.innerHTML = `<div class="prod"><span>Cart is Empty Please add Items</span></div>`;
+}
 
-let foo = () => { localStorage.removeItem('cart'); }
+const foo = () => {
+    for (const key in cart) {
+        document.getElementById('div' + key).innerHTML = `<button class="btn cart" id="${key}">Add to Cart</button>`;
+        delete cart[key];
+    }
+    updateCart();
+}
---
+++ jkart/shop/static/shop/JS/checkout.js
@@ -0,0 +1,38 @@
+// if minus button is clicked
+$('.review').on('click', 'button.minus', function () {
+    let id = this.id.slice(5,);
+    cart[id][0] = Math.max(0, (cart[id][0] - 1));
+    if (!cart[id][0]) {
+        document.getElementById('div' + id).innerHTML = `<button class="btn cart" id="${id}">Removing</button>`;
+        setTimeout(()=>{
+            if(cart[id] == undefined)document.getElementById(id).remove();
+        },2000)
+        delete cart[id];
+    }
+    else document.getElementById(`qty${id}`).innerHTML = cart[id][0];
+    localStorage.setItem('cart', JSON.stringify(cart));
+    updateCart();
+});
+
+// if plus button is clicked
+$('.review').on('click', 'button.plus', function () {
+    let id = this.id.slice(4,);
+    cart[id][0] += 1;
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.getElementById(`qty${id}`).innerHTML = cart[id][0];
+    updateCart();
+});
+
+const fillCheckoutPage = () => {
+    let x = document.getElementById('review');
+    for (const key in cart) {
+        let review = `<div class="list-product" id="${key}">
+            <div class="prod-name">${cart[key][1]}</div>
+            <div class="divpr" id="div${key}">
+            <button class='btn minus' id='minus${key}'>-</button><span id='qty${key}' class='value'>${cart[key][0]}</span><button class='btn plus' id='plus${key}'>+</button>
+            </div>
+        </div>`
+        x.innerHTML += review;
+
+    }
+}
