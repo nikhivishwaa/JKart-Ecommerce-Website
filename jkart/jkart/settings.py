3759162e5a3f081d4702f4e8f78e463cceed784f
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-12-21 21:46:52 +0530

    moving towards tracker page

--- jkart/jkart/settings.py
+++ jkart/jkart/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'jkart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["jkart/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
